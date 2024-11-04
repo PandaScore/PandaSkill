c77bfa312863c045e46ab87a00c408898d235512
Maxime De Bois <maxime.debois@pandascore.co>
2024-11-04 11:48:42 +0100

    display data

--- pandaskill/app/streamlit_app.py
+++ pandaskill/app/streamlit_app.py
@@ -1,3 +1,13 @@
 import streamlit as st
+import pandas as pd
 
 st.title("PandaSkill")
+
+df = pd.read_csv("pandaskill/artifacts/data/data_2019-09-15_2024-09-15.csv")
+
+
+df = df.loc[:,['game_id', 'player_id', 'date', 'match_id', 'tournament_id',
+       'tournament_name', 'serie_id', 'serie_name', 'league_id', 'league_name', 'region',
+       'team_id', 'team_name', 'team_acronym', 'player_name', 'role', 'win']]
+
+st.dataframe(df)
